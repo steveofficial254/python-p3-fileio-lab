62f25c7f8cd9be51c3b2d03660a94b7110a733b2
stephen mburu <stephenmburu8509@gmail.com>
2025-07-14 12:32:26 +0300

    learning file IO

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
-    pass
+    full_file_name = f"{file_name}.txt"
+    with open(full_file_name, 'w') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    full_file_name = f"{file_name}.txt"
+    with open(full_file_name, 'a') as file:
+        file.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    full_file_name = f"{file_name}.txt"
+    with open(full_file_name, 'r') as file:
+        return file.read()
